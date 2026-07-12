6cec4c2553716e97ca1150215229d4bf7a7879e6
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "vpn_gateways" {
-  description = "All vpn_gateway resources"
-  value       = azurerm_vpn_gateway.vpn_gateways
-}
 output "vpn_gateways_bgp_route_translation_for_nat_enabled" {
-  description = "List of bgp_route_translation_for_nat_enabled values across all vpn_gateways"
-  value       = [for k, v in azurerm_vpn_gateway.vpn_gateways : v.bgp_route_translation_for_nat_enabled]
+  description = "Map of bgp_route_translation_for_nat_enabled values across all vpn_gateways, keyed the same as var.vpn_gateways"
+  value       = { for k, v in azurerm_vpn_gateway.vpn_gateways : k => v.bgp_route_translation_for_nat_enabled }
 }
 output "vpn_gateways_bgp_settings" {
-  description = "List of bgp_settings values across all vpn_gateways"
-  value       = [for k, v in azurerm_vpn_gateway.vpn_gateways : v.bgp_settings]
+  description = "Map of bgp_settings values across all vpn_gateways, keyed the same as var.vpn_gateways"
+  value       = { for k, v in azurerm_vpn_gateway.vpn_gateways : k => v.bgp_settings }
 }
 output "vpn_gateways_ip_configuration" {
-  description = "List of ip_configuration values across all vpn_gateways"
-  value       = [for k, v in azurerm_vpn_gateway.vpn_gateways : v.ip_configuration]
+  description = "Map of ip_configuration values across all vpn_gateways, keyed the same as var.vpn_gateways"
+  value       = { for k, v in azurerm_vpn_gateway.vpn_gateways : k => v.ip_configuration }
 }
 output "vpn_gateways_location" {
-  description = "List of location values across all vpn_gateways"
-  value       = [for k, v in azurerm_vpn_gateway.vpn_gateways : v.location]
+  description = "Map of location values across all vpn_gateways, keyed the same as var.vpn_gateways"
+  value       = { for k, v in azurerm_vpn_gateway.vpn_gateways : k => v.location }
 }
 output "vpn_gateways_name" {
-  description = "List of name values across all vpn_gateways"
-  value       = [for k, v in azurerm_vpn_gateway.vpn_gateways : v.name]
+  description = "Map of name values across all vpn_gateways, keyed the same as var.vpn_gateways"
+  value       = { for k, v in azurerm_vpn_gateway.vpn_gateways : k => v.name }
 }
 output "vpn_gateways_resource_group_name" {
-  description = "List of resource_group_name values across all vpn_gateways"
-  value       = [for k, v in azurerm_vpn_gateway.vpn_gateways : v.resource_group_name]
+  description = "Map of resource_group_name values across all vpn_gateways, keyed the same as var.vpn_gateways"
+  value       = { for k, v in azurerm_vpn_gateway.vpn_gateways : k => v.resource_group_name }
 }
 output "vpn_gateways_routing_preference" {
-  description = "List of routing_preference values across all vpn_gateways"
-  value       = [for k, v in azurerm_vpn_gateway.vpn_gateways : v.routing_preference]
+  description = "Map of routing_preference values across all vpn_gateways, keyed the same as var.vpn_gateways"
+  value       = { for k, v in azurerm_vpn_gateway.vpn_gateways : k => v.routing_preference }
 }
 output "vpn_gateways_scale_unit" {
-  description = "List of scale_unit values across all vpn_gateways"
-  value       = [for k, v in azurerm_vpn_gateway.vpn_gateways : v.scale_unit]
+  description = "Map of scale_unit values across all vpn_gateways, keyed the same as var.vpn_gateways"
+  value       = { for k, v in azurerm_vpn_gateway.vpn_gateways : k => v.scale_unit }
 }
 output "vpn_gateways_tags" {
-  description = "List of tags values across all vpn_gateways"
-  value       = [for k, v in azurerm_vpn_gateway.vpn_gateways : v.tags]
+  description = "Map of tags values across all vpn_gateways, keyed the same as var.vpn_gateways"
+  value       = { for k, v in azurerm_vpn_gateway.vpn_gateways : k => v.tags }
 }
 output "vpn_gateways_virtual_hub_id" {
-  description = "List of virtual_hub_id values across all vpn_gateways"
-  value       = [for k, v in azurerm_vpn_gateway.vpn_gateways : v.virtual_hub_id]
+  description = "Map of virtual_hub_id values across all vpn_gateways, keyed the same as var.vpn_gateways"
+  value       = { for k, v in azurerm_vpn_gateway.vpn_gateways : k => v.virtual_hub_id }
 }
 
